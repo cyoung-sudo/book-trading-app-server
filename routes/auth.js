a2b71509aa73603a57f880b9ca213a199fc47fbf
Christopher Young <christopheryoung@MacBook123.local>
2023-01-19 14:20:58 -0800

    added user authentication

---
+++ routes/auth.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const authRoutes = express.Router();
+// Authentication
+const passport = require("passport");
+// Models
+const User = require("../models/userModel");
+// Encryption
+const bcrypt = require("bcryptjs");
+
+//----- Signup user
+authRoutes.post("/api/auth/signup", (req, res) => {
+  // Encrypt password
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(req.body.password, salt, (err, hash) => {
+      // Create new user
+      let newUser = new User({
+        username: req.body.username,
+        password: hash
+      });
+
+      // Save user
+      newUser.save()
+      .then(savedDoc => {
+        res.json({ success: true });
+      })
+      .catch(err => {
+        console.log(err);
+        res.json({ 
+          success: false,
+          message: "Username has been taken"
+        });
+      });
+    });
+  });
+});
+
+//----- Login user
+authRoutes.post("/api/auth/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if(err) next(err);
+
+    // Invalid login
+    if(!user) {
+      res.json({
+        success: false,
+        message: info.message
+      });
+    } 
+
+    // Successful login
+    if(user) {
+      req.logIn(user, err => {
+        if(err) next(err);
+        res.json({
+          success: true,
+          user
+        });
+      });
+    }
+  })(req, res, next);
+});
+
+//----- Logout user
+authRoutes.post("/api/auth/logout", (req, res) => {
+  if(req.user) {
+    res.json({
+      success: true,
+      user: req.user
+    });
+  } else {
+    res.json({
+      success: false,
+      message: "No active session"
+    });
+  }
+});
+
+//----- Retrieve authenticated user
+authRoutes.get("/api/auth/user", (req, res) => {
+  if(req.user) {
+    res.json({
+      success: true,
+      user: req.user
+    });
+  } else {
+    res.json({
+      success: false,
+      message: "No active session"
+    });
+  }
+});
+
+module.exports = authRoutes;
