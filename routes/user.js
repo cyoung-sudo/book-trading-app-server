dd5c78ebfdc22bd143107cd3ddadfb7b5cc82cbe
Christopher Young <christopheryoung@MacBook123.local>
2023-01-20 10:20:49 -0800

    updated user routes

--- routes/user.js
+++ routes/user.js
@@ -16,4 +16,30 @@ userRoutes.route("/api/user")
   .catch(err => console.log(err));
 });
 
+userRoutes.route("/api/user/:id")
+//----- Retrieve given user
+.get((req, res) => {
+  User.findById(req.params.id)
+  .then(doc => {
+    if(doc) {
+      res.json({
+        success: true,
+        user: doc
+      });
+    } else {
+      res.json({
+        success: false,
+        message: "User not found"
+      });
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.json({
+      success: false,
+      message: "Invalid userID"
+    })
+  });
+});
+
 module.exports = userRoutes;
